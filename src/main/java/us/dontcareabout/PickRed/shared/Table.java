1f3799e43be7b0e24c7b757a946839b20ff61b02
MontyPan <monty.pan@dontcareabout.us>
2019-06-08 21:05:19 +0800

    增加：IDE 產生的 equals() 與 hashCode()

--- src/main/java/us/dontcareabout/PickRed/shared/Table.java
+++ src/main/java/us/dontcareabout/PickRed/shared/Table.java
@@ -11,9 +11,9 @@ public class Table implements Serializable {
 
 	private String id;
 	private Player master;
-	private ArrayList<Player> playerList = new ArrayList<>();
+	private List<Player> playerList = new ArrayList<>();
 	private int max;
-	private Date createTime;
+	private long createTime;
 
 	//just for GWT RPC
 	Table() {}
@@ -23,7 +23,7 @@ public class Table implements Serializable {
 		this.master = master;
 		this.max = max;
 		join(master);
-		createTime = new Date();
+		createTime = new Date().getTime();
 	}
 
 	public boolean join(Player player) {
@@ -61,7 +61,53 @@ public class Table implements Serializable {
 		return master;
 	}
 
-	public Date getCreateTime() {
+	public long getCreateTime() {
 		return createTime;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setMaster(Player master) {
+		this.master = master;
+	}
+
+	public void setMax(int max) {
+		this.max = max;
+	}
+
+	public void setCreateTime(long createTime) {
+		this.createTime = createTime;
+	}
+
+	public void setPlayerList(List<Player> list) {
+		playerList.clear();
+		playerList.addAll(list);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Table other = (Table) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
